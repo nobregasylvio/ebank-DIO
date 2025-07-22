c30a624f97e956297977a8dfb74c239b9e9c1e83
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2025-07-22 01:44:35 -0300

    test: adiciona codigo para testar as funcionalidades iniciais

--- src/main/java/dev/nobregasylvio/Main.java
+++ src/main/java/dev/nobregasylvio/Main.java
@@ -4,6 +4,16 @@ package dev.nobregasylvio;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Cliente sylvio = new Cliente("Sylvio");
 
+        ContaCorrente cc = new ContaCorrente(sylvio);
+        ContaPoupanca poupanca = new ContaPoupanca(sylvio);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        System.out.println();
+        poupanca.imprimirExtrato();
     }
 }
