f6d92c98469bc1887922ee867512bdfbd9a88ef2
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2025-07-22 01:41:50 -0300

    feat: adicionando o metodo de imprimir extrato

--- src/main/java/dev/nobregasylvio/Conta.java
+++ src/main/java/dev/nobregasylvio/Conta.java
@@ -33,4 +33,11 @@ public abstract class Conta {
         this.sacar(valor);
         contaDestino.depositar(valor);
     }
+
+    protected void imprimirInfosComuns() {
+        System.out.printf("Titular: %s\n", this.cliente.getNome());
+        System.out.printf("Agencia: %d\n", this.agencia);
+        System.out.printf("Numero: %d\n", this.numero);
+        System.out.printf("Saldo: %.2f\n", this.saldo);
+    }
 }
